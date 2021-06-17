a092757e4ee242b30189f9db0e8de3a368a1f2ab
Linh Tran <linh@xseed.tech>
2021-06-17 09:58:33 +0000

    add function to utilize PowerShell to run as background app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express()
 const port = 1704
 const {exec} = require("child_process")
+const fs = require('fs')
 
 app.get("/gameStreamAutoPair/:pin", (req, res)=>{
     exec("gameStreamAutoPair.exe " + req.params.pin, (err, stdout, stderr)=>{
@@ -16,10 +17,39 @@ app.get("/gameStreamAutoPair/:pin", (req, res)=>{
         }
         res.json({
             status: "ok",
+  err,stdout,stderr
         })
     })
 })
 
+
+
 app.listen(port, ()=>{
     console.log(`App listening at http://localhost:${port}`)
+    hideSelf()
 })
+
+function hideSelf() {
+
+    let powershellScript = `
+    Add-Type -Name Window -Namespace Console -MemberDefinition '
+    [DllImport("Kernel32.dll")]
+    public static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+    '
+
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+    `;
+
+    let workingDir = process.cwd();
+    let tempfile = `${workingDir}\\temp.ps1`;
+    fs.writeFileSync(tempfile, powershellScript);
+
+    //a little convoluted to get around powershell script execution policy (might be disabled)
+    require('child_process').execSync(`type .\\temp.ps1 | powershell.exe -noprofile -`, {stdio: 'inherit'});
+    fs.unlinkSync(tempfile); //delete temp file
+}
